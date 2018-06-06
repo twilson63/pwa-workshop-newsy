63d00189421b02aca8aa144f21ea1ab5c08ce9b6
Tom Wilson <tom@jackhq.com>
2018-06-06 19:12:05 -0400

    removed app

--- app/src/theme.js
+++
@@ -1,9 +0,0 @@
-import { createMuiTheme } from '@material-ui/core/styles'
-
-import purple from '@material-ui/core/colors/purple'
-
-export default createMuiTheme({
-  palette: {
-    primary: purple
-  }
-})
